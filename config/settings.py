0ad30ba500d4d64577350ae026129870349bc999
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2025-10-28 17:39:37 -0400

    chore:agregar host despledado

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smartventassi22p-production.up.railway.app']
 
 
 # Application definition
